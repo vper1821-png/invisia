014e241e13670255c74128c52b59a6eab27cd6ea
vper1821-png <vper1821@gmail.com>
2026-05-06 18:24:21 -0400

    Update entrypoint.sh

--- sftp/entrypoint.sh
+++ sftp/entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "SFTP shared hosting (single site, 3 admins)"
+echo "SFTP shared hosting"
 
 addgroup -S web || true
 
-# crear usuarios administradores
 for u in kp dev admin; do
   adduser -D -h /var/www/html -u $(shuf -i 1001-2000 -n 1) $u
+
+  # passwords consistentes (NO del Deployment)
   echo "$u:Password$u" | chpasswd
+
   addgroup $u web || true
 done
 
-# 🔥 TODO EL SITIO ES COMPARTIDO
 chown -R root:web /var/www/html
 chmod -R 2775 /var/www/html
 
 /usr/sbin/sshd -D -e
-
-
