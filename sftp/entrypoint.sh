867a357b7ece348fb5adaf603a8eddeeeb8cc6df
vper1821-png <vper1821@gmail.com>
2026-05-06 16:32:07 -0400

    Update entrypoint.sh

--- sftp/entrypoint.sh
+++ sftp/entrypoint.sh
@@ -3,25 +3,35 @@ set -e
 
 echo "Iniciando SFTP multiusuario..."
 
-# crear usuarios dinámicamente
+# cargar usuarios desde archivo o fallback
+USERS_FILE="/users.conf"
+
+if [ -f "$USERS_FILE" ]; then
+  echo "Usando users.conf"
+else
+cat <<EOF > /tmp/users.conf
+kp:1001:PasswordKP
+dev:1002:PasswordDEV
+admin:1003:PasswordADMIN
+EOF
+  USERS_FILE="/tmp/users.conf"
+fi
+
 while IFS=: read -r user uid pass; do
   echo "Creando usuario $user ($uid)"
 
   adduser -D -u $uid $user
   echo "$user:$pass" | chpasswd
 
-  mkdir -p /home/$user
-  chown -R $user:$user /home/$user
-  chmod 755 /home/$user
+  # 🔥 CLAVE: todos trabajan en /data (NO /home)
+  mkdir -p /data/$user
+  chown -R $user:$user /data/$user
+  chmod 775 /data/$user
 
-done << EOF
-kp:1001:PasswordKP
-dev:1002:PasswordDEV
-admin:1003:PasswordADMIN
-EOF
+done < "$USERS_FILE"
 
-# permisos globales RWX (Longhorn)
-chown -R root:root /data || true
+# 🔥 PERMISOS COMPARTIDOS CON WEB
+chown -R root:www-data /data || true
 chmod -R 775 /data || true
 
 /usr/sbin/sshd -D -e
